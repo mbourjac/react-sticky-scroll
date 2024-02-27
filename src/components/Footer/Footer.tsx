38b52d2955f065b468a2c53e0aa1d6f2a7407fb3
Michaël Bourjac <michael.bourjac@gmail.com>
2024-02-27 19:11:49 +0100

    feat: create footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import styles from './Footer.module.scss';
+
+export const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <h2 className={styles.heading}>
+        Quis earum veritatis error dolorum consequuntur nulla neque porro ut
+        quos
+      </h2>
+    </footer>
+  );
+};
